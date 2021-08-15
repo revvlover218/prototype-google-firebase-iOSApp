9e1f0f178736512644a845628fa0d4ec1dc2a5b7
Reshal Luchman (ZA) <Reshal.Luchman@absa.africa>
2021-08-15 22:30:52 +0200

    Changed main view controller to HomeScreen.

---
+++ FirebasePrototype/FirebasePrototype/Home Screen/HomeScreenViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeScreenViewController.swift
+//  FirebasePrototype
+//
+//  Created by Reshal Luchman on 2021/08/15.
+//
+
+import UIKit
+
+class HomeScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
